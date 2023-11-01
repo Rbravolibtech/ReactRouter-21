1dfa7b6676cf999e8e8023458d219e1e83516ae7
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-11-01 13:16:48 -0600

    using dynamic routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,17 @@
 function ProductsPage() {
-	return <h1>THE PRODUCTS PAGE</h1>;
+	return (
+		<>
+			<h1>THE PRODUCTS PAGE</h1>
+
+			<ul>
+				<li>PRODUCT 1</li>
+				<li>PRODUCT 2</li>
+				<li>PRODUCT 3</li>
+				<li>PRODUCT 4</li>
+				<li>PRODUCT 5</li>
+			</ul>
+		</>
+	);
 }
 
 export default ProductsPage;
