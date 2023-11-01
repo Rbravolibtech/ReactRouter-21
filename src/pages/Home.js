660de17e8c7355161b0f2d1769a674fa1b0a82af
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-11-01 11:00:32 -0600

    added a link to products page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-	return <h1>MY HOME PAGE</h1>;
+	return (
+		<>
+			<h1>MY HOME PAGE</h1>
+			<p>
+				GO TO <Link to="/products">THE LIST OF PRODUCTS </Link>
+			</p>
+		</>
+	);
 }
 
 export default HomePage;
